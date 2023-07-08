3cc960c76f154572fdd3b4113fac80d65b542053
AshRahman <ashfaqurrahmanrahat@outlook.com>
2023-07-08 10:52:22 +0600

    added class

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,20 +1,25 @@
 import time
 
 
-def single_pomodoro(t):
-    t = t * 3600  # makes the hour into seconds
-    while t:
-        secs = t % 60  # makes the second not exceed 60 mark
-        mins = int(t / 60) % 60  # makes the minute not exceed 60 mark
-        hours = int(t / 3600)
-        timer = f"{hours:02}:{mins:02}:{secs:02}"
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-        #####if (mins=)
-    print("Times Up")
+class Timer:
+    def __init__(self, t) -> None:
+        self.t = t
+
+    def single_pomodoro(self, t):
+        t = t * 3600  # makes the hour into seconds
+        while t:
+            secs = t % 60  # makes the second not exceed 60 mark
+            mins = int(t / 60) % 60  # makes the minute not exceed 60 mark
+            hours = int(t / 3600)
+            timer = f"{hours:02}:{mins:02}:{secs:02}"
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
+            #####if (mins=)
+        print("Times Up")
 
 
-t = input("Choose the practice time Duration in hour:")
+t = int(input("Choose the practice time Duration in hour:"))
 
-single_pomodoro(int(t))
+timer1 = Timer()
+timer1.single_pomodoro(t)
