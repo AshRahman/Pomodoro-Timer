1a3e366f9cbfd611952f3d47a45e81a4b509c68e
AshRahman <ashfaqurrahmanrahat@outlook.com>
2023-07-27 18:23:36 +0600

    doc

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -8,10 +8,16 @@ class Timer:
     def single_pomodoro(self, t):
         t = t * 3600  # makes the hour into seconds
         while t:
-            """an if else statement needs to be written for 35 and 5 min
-            mark where it will then go to the break_start funstion to star the
+            """an if else statement needs to be written for 25 and 5 min
+            mark where it will then go to the break_start function to star the
             break procedure
+            25 min = 1500 sec
+            55 min = 3300 sec + 1800
+            1 hour 25 min = 5100 sec + 1800
+            1 hour 55 min =  6900 sec
+            
             """
+            if()
             secs = t % 60  # makes the second not exceed 60 mark
             mins = int(t / 60) % 60  # makes the minute not exceed 60 mark
             hours = int(t / 3600)
